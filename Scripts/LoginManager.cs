dc8e64b3ae76b3db14c7b9da9d2758b35b083c81
D1nand <dinandjurgen@gmail.com>
2024-11-27 17:55:57 +0100

    Dl-task-126/127
    Vertification works and logout works now as well:)

--- Scripts/LoginManager.cs
+++ Scripts/LoginManager.cs
@@ -7,7 +7,6 @@ using TMPro; // Required for TMP_InputField
 public class LoginManager : MonoBehaviour
 {
     private string apiUrl = "http://localhost/codenamesAPI/Login.php"; // PHP Login URL
-    private string checkAccessUrl = "http://localhost/codenamesAPI/CheckAccess.php"; // PHP Check Access URL
 
     [System.Serializable]
     public class LoginData
@@ -67,7 +66,6 @@ public class LoginManager : MonoBehaviour
                 sessionToken = response.token;
                 PlayerPrefs.SetString("SessionToken", sessionToken); // Save session token to PlayerPrefs
                 PlayerPrefs.Save();
-                Debug.Log("Login succesvol.");
                 SceneManager.LoadScene("DashboardTeachers"); // Load Teacher's Dashboard
             }
             else
@@ -99,6 +97,8 @@ public class LoginManager : MonoBehaviour
         {
             passwordInputField.text = "";
         }
+    }
+
     // Logout function to clear the session token and redirect to the login scene
     public void Logout()
     {
