f404e9a998218d2945fc430771573ea701a13755
D1nand <dinandjurgen@gmail.com>
2024-12-03 17:55:12 +0100

    dl-task-128

--- Scripts/AddGameManager.cs
+++ Scripts/AddGameManager.cs
@@ -1,14 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class AddGameManager : MonoBehaviour
 {
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
+    public TMP_InputField GameNaamInput;
+    public TMP_InputField TrefwoordInput;
+    public TMP_InputField BetekenisInput;
 
     // Update is called once per frame
     void Update()
